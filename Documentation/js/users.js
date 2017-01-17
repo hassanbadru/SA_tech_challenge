fb49762c44817fc1246f3d82b09e6a04b84d3d04
Hassan Badru <hassan.badru@gmail.com>
2017-01-17 09:28:28 -0500

    done

---
+++ Documentation/js/users.js
@@ -0,0 +1,163 @@
+{
+   "data": [
+       /*=========*/
+       /* USER 1 */
+      /*========*/  
+      {
+         "shoeID": "001",
+         "email": "adam@sd.com",
+         "password": "123",
+         "name": "Adam Goldstein",
+         "prestaurant":"Cupcake Deli",
+         "location":"Cambridge, MA",
+         "avatar": "img/photo1.jpg", 
+         "steps": 100000, /* 100,000 */
+         "progress": {"stepsToday":3500,
+                      "dailyGoal":4000
+                     },
+         "hist":{"all":[    {
+                                "Date":"Yesterday",
+                                "Steps":1073,
+                                "Miles":0.54
+                            },
+                            {
+                                "Date":"May 6, 2016",
+                                "Steps":4338,
+                                "Miles":2.17
+                            },
+                            {
+                                "Date":"Yesterday",
+                                "Steps":4600,
+                                "Miles":2.30
+                            },
+                        ],
+                    
+                 "twelve":[ {
+                                "Date":"Yesterday",
+                                "Steps":1073,
+                                "Miles":0.54
+                            },
+                            {
+                                "Date":"Yesterday",
+                                "Steps":1073,
+                                "Miles":0.54
+                            }
+                        ],
+                "six":{
+                                "Date":"Yesterday",
+                                "Steps":1073,
+                                "Miles":0.54
+                      },
+                        
+             
+         }
+      },
+       
+       /*=========*/
+       /* USER 2 */
+      /*========*/   
+      {
+         "shoeID": "002",
+         "email": "emily@sd.com",
+         "password": "123",
+         "name": "Emily Malina",
+         "prestaurant":"Croissant Express",
+         "location":"Boston, MA",
+         "avatar": "img/photo2.jpg", 
+         "steps": 2000000, /* 2,000,000 */
+         "progress": {"stepsToday":2000,
+                      "dailyGoal":5000
+                     },
+         "hist":{"all":[    {
+                                "Date":"Yesterday",
+                                "Steps":"1073",
+                                "Miles":"0.54"
+                            },
+                            {
+                                "Date":"May 6, 2016",
+                                "Steps":4338,
+                                "Miles":2.17
+                            },
+                            {
+                                "Date":"Yesterday",
+                                "Steps":4600,
+                                "Miles":2.30
+                            },
+                        ],
+                    
+                 "twelve":[ {
+                                "Date":"Yesterday",
+                                "Steps":1073,
+                                "Miles":0.54
+                            },
+                            {
+                                "Date":"Yesterday",
+                                "Steps":1073,
+                                "Miles":0.54
+                            }
+                        ],
+                "six":{
+                                "Date":"Yesterday",
+                                "Steps":1073,
+                                "Miles":0.54
+                      },
+                        
+             
+         }
+      },
+          
+        /*=========*/
+       /* USER 3 */
+      /*========*/   
+      {
+         "shoeID": "003",
+         "email": "hassan@sd.com",
+         "password": "123",
+         "name": "Hassan Badru",
+         "prestaurant":"The Cake Factory",
+         "location":"New York, NY",
+         "avatar": "img/photo3.jpg", 
+         "steps": 50000, /* 50,000 */
+         "progress": {"stepsToday":1000,
+                      "dailyGoal":2000
+                     },
+         "hist":{"all":[    {
+                                "Date":"Yesterday",
+                                "Steps":"1073",
+                                "Miles":"0.54"
+                            },
+                            {
+                                "Date":"May 6, 2016",
+                                "Steps":"4,338",
+                                "Miles":"2.17"
+                            },
+                            {
+                                "Date":"Yesterday",
+                                "Steps":"4,600",
+                                "Miles":"2.30"
+                            },
+                        ],
+                    
+                 "twelve":[ {
+                                "Date":"Yesterday",
+                                "Steps":"1073",
+                                "Miles":"0.54"
+                            },
+                            {
+                                "Date":"Yesterday",
+                                "Steps":"1073",
+                                "Miles":"0.54"
+                            }
+                        ],
+                "six":{
+                                "Date":"Yesterday",
+                                "Steps":"1073",
+                                "Miles":"0.54"
+                      },
+                        
+             
+         }
+      }
+   ]
+}
+
